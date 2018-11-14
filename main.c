096bbd24b2af9d0d3895d1f95630c3d149ffdc42
김주홎 <gyong0812@naver.com>
2018-11-14 14:18:10 +0900

    try 2

--- main.c
+++ main.c
@@ -1,56 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> 
+#include <time.h>
 
-#define N 4 //NxN BINGO GAME
-
+#define N 4  
 
 void initiate_bingo(int arr[N][N]);
 void print_bingo(int arr[N][N]);
 
 
 int main(void)
-{	
-	printf("START BINGO GAME\n");
-	
-	int arr[N][N] ={0}; //배열 선언  
-	
-	initiate_bingo(arr); 	//빙고판 생성  
-	print_bingo(arr); 		//빙고판 출력 
-	
-	return 0; 
+{
+   printf("START BINGO GAME\n");
+   int arr[N][N];    	  	 //배열 선언
+
+   initiate_bingo(arr);      //빙고판 생성  
+   print_bingo(arr);         //빙고판 출력
+
+   return 0;
 }
 
 
 
 void initiate_bingo(int arr[N][N])
 {
-	int row; //빙고테이블 가로	 
-	int col; //빙고테이블 세로  
-	
-	srand((unsigned)time(NULL)); //난수만들기  
-	
-	for(row=0;row<N;row++)
-	{
-		for(col=0; col<N; col++)
-		{
-			arr[row][col]=1+(rand()%(N*N));
-		}
-	}
+   int row;
+   int col;
+    
+   srand((unsigned int)time(NULL));   //난수 초기화
+   
 
-
-void print_bingo(int arr[N][N]) 
-{	
-	int row ;
-	int col ;
-	 
-	for(row=0; row<N; row++)
-	{
-		for(col=0; col<N; col++)
-		{
-			printf("%d ",arr[row][col]);
-		}
-	}
+	for(row=0; row<N; row++)	//NxN 배열에 넣기위한 반복문
+   	{
+	   for (col=0; col<N; col++)
+      	{
+        	arr[row][col] = 1 + (rand() % ((N)*(N)));   // 1 ~ NxN 사이의 난수생성과 입력 
+      	}
+   	}
 }
 
-
+void print_bingo(int arr[N][N])
+{   
+   int row;
+   int col;
+   
+   for (row=0; row<N; row++)
+   {
+      for (col=0; col<N; col++)
+     {
+        printf("%d\t", arr[row][col]);      //빙고판 출력
+     }
+      printf("\n"); //가로줄 다 쓰면 아랫줄로 이동                     
+   }
+}
