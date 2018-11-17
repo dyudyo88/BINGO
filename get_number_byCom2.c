dd32202193058f161e674d070653267fb38893c8
김주홎 <gyong0812@naver.com>
2018-11-17 17:27:09 +0900

    2312312

---
+++ get_number_byCom2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 4   // NxN bingo game
+#define M 2 
+#define OVERLAPED 1
+#define UNOVERLAPED 0
+
+
+
+int row, col;
+int num;
+
+
+
+int get_number_byCom2(int num)
+{	
+	int i,j;
+	num = 1 + (rand() % ((N)*(N)));
+	int array[N*N];
+	int arr[N][N];
+	
+
+	while(1)
+	{
+	
+		if(array[num]=0)
+		{
+			arr[N][N]=num;
+			array[num] =1;
+		}
+		break;
+
+	}
+	
+	return num;
+} 
+
+
